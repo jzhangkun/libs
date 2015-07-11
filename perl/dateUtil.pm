bd61fc063de6124bac317dc160692c8318a6eb0e
jzhangkun <jzhangkun1986@gmail.com>
2015-07-11 10:51:12 +0800

    timegm to replace timelocal

--- perl/dateUtil.pm
+++ perl/dateUtil.pm
@@ -1,13 +1,13 @@
 package dateUtil;
 use iBase;
-use Time::Local qw(timelocal);
+use Time::Local qw(timegm);
 use POSIX qw(strftime);
 
 sub date_to_iso8601 {
     my $epoch;
     if ($_[0] =~ m{(\d{4})\/(\d{2})\/(\d{2}) (\d{2}):(\d{2}):(\d{2})}) {
         #Date Format as 'yyyy/mm/dd hh24:mi:ss'
-        $epoch = timelocal($6, $5, $4, $3, $2-1, $1-1900);
+        $epoch = timegm($6, $5, $4, $3, $2-1, $1-1900);
     }
     return unless $epoch;
     (my $tz = strftime("%z", localtime($epoch))) =~ s/(\d\d)(\d\d)/$1:$2/;
